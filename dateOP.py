d77b48b89192c486e6596df9a5b21c557210b327
raynasser <rnff-333@hotmail.com>
2024-06-23 14:09:13 +0300

    DAY#9 LAB#2

---
+++ dateOP.py
@@ -0,0 +1,11 @@
+# A function that when called prints the current date.
+
+
+
+from datetime import datetime
+
+def current_date():
+    return datetime.now()
+
+
+# print(current_date())
---
+++ main.py
@@ -0,0 +1,8 @@
+# - import dateOP
+# - call the function that print the current date
+
+
+from dateOP import current_date
+
+
+print(current_date())
