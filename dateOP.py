2550a9f0805564df553eb18d84304d5d915690c4
bushraali98 <bm-bisho-90210@hotmail.com>
2024-06-23 20:52:11 +0300

    Lab solution done

---
+++ dateOP.py
@@ -0,0 +1,6 @@
+from datetime import date
+
+def get_current_date():
+  print("The current date:", date.today())
+
+get_current_date()
---
+++ main.py
@@ -0,0 +1,5 @@
+# importing the module
+import dateOP
+
+# calling the function from the module
+dateOP.get_current_date
