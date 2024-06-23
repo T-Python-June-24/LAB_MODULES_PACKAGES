f52e4c9c7a63471b5935531392434224edcbfecb
Sarah <monifsarah@gmail.com>
2024-06-23 13:57:09 +0300

    Solved LAB_MODULES_PACKAGES

---
+++ dateOP.py
@@ -0,0 +1,7 @@
+'''
+A function that when called prints the current date
+'''
+from datetime import date
+def currentdate():
+ today = date.today()
+ return today
---
+++ main.py
@@ -0,0 +1,3 @@
+import dateOP
+
+print(dateOP.currentdate())
